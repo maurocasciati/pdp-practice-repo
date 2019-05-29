fedcaa477816f6d0f591e5680882b448d40a4aef
Mauro Nahuel Casciati <31457612+maurocasciati@users.noreply.github.com>
2019-05-28 21:20:35 -0300

    fix: agrego clase a intro-logico

--- intro-logico.pl
+++ intro-logico.pl
@@ -1,3 +1,4 @@
+% VS PLUGIN: VSC-Prolog
 % ------- Ejemplos de la primera clase 
 
 % Set de datos. Si no esta aca, no va a dar true 
@@ -32,4 +33,64 @@ esRico(ana).
 % que no sea ana, ya que no se fija si es o no perona.
 % Entonces lo cambiamos por:
 esPobre(Persona):- esPersona(Persona), not(esRico(Persona)).
+
+
+% Calculos con IS (en vez de =)
+precio(cafe,25).
+precio(auriculares,250).
+precio(mouse,400).
+
+dinero(viotti,200).
+dinero(devoto,1030).
+
+compra(Persona,Producto):-
+  dinero(Persona,Dinero),
+  precio(Producto,Precio),
+  Dinero >= Precio,
+  Cantidad is Dinero / Precio.
+
+% Inversivilidad: Conviene usarlo porque permite usar las cosas de distinta manera. buscar
+
+% ------------ Listas:
+% LENGTH
+% -/ length(1,2,3,4,5) %devuelve 5
+% -/ length(X,5) %devuelve listas con 5 elementos que son cualquier cosa
+
+% Teoria: Los predicados estan cuantificados existencialmente (o sea, existe tal cosa, lo que no esta en predicados no)
+% FORALL
+profesion(juan,herrero).
+profesion(ana,carpintera).
+profesion(pedro,herrero).
+
+tieneCuchillo(juan,palo).
+tieneCuchillo(ana,metal).
+tieneCuchillo(pedro,metal).
+
+enCasaDeHerreroCuchilloDePalo(Fulano?):-
+  profesion(Fulano, herrero),
+  tieneCuchillo(Fulano, palo).
+  
+% enCasaDeHerreroCuchilloDePalo(X) me devuelve juan
+% enCasaDeHerreroCuchilloDePalo(_) me devuelve true porque existe aunque sea 1
+
+enCasaDeHerreroCuchilloDePaloSiempre:-
+  forall(profesion(Fulano, herrero),
+    tieneCuchillo(Fulano, palo)). %devuelve true si siempre se cumple    
+
+% FORALL CON MAS DE DOS PARAMETROS
+alQueMadrugaDiosLoAyuda:-
+  forall(madruga(Fulano),
+        (loAyuda(Dios,Fulano),esDios(Dios))).
+
+madruga(juan).
+madruga(ana).
+loAyuda(juan,yaleh).
+loAyuda(ana,diablo).
+esDios(ala).
+esDios(yaleh).
+    
+% Ejercicio: hacer un nuevo refran:
+
+
+
 
